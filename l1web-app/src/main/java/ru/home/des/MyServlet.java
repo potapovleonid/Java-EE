5c3f5e7fc1a1dcca0847ad1adafebd39f91be8bf
Leonid <kissmykrabeskill@gmail.com>
2021-02-01 23:30:28 +0500

    Lesson 01

---
+++ l1web-app/src/main/java/ru/home/des/MyServlet.java
@@ -0,0 +1,42 @@
+package ru.home.des;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+public class MyServlet implements Servlet {
+
+    private static final Logger logger = LoggerFactory.getLogger(MyServlet.class);
+
+    private ServletConfig servletConfig;
+
+    @Override
+    public void init(ServletConfig servletConfig) throws ServletException {
+        logger.info("Initial procces MyServlet");
+        this.servletConfig = servletConfig;
+    }
+
+    @Override
+    public ServletConfig getServletConfig() {
+        return servletConfig;
+    }
+
+    @Override
+    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+        logger.info("New request to MyServlet");
+        servletResponse.getWriter().println("<h1> Hello from my servlet</h1>");
+    }
+
+    @Override
+    public String getServletInfo() {
+        logger.info("get info from MyServlet");
+        return null;
+    }
+
+    @Override
+    public void destroy() {
+        logger.info("Delete MyServlet");
+    }
+}
---
+++ l1web-app/src/main/webapp/index.jsp
@@ -0,0 +1,6 @@
+<html>
+<body>
+<a href="l1web-app/less1web-app">Test page</a>
+<h2>Hello World!</h2>
+</body>
+</html>
