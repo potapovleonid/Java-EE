d4e7bb0bcb86357d923924fd404c19c84b29d742
Leonid <kissmykrabeskill@gmail.com>
2021-02-17 22:08:38 +0500

    Init Lesson_03

--- l1web-app/src/main/java/ru/home/des/HeaderServlet.java
+++ l1web-app/src/main/java/ru/home/des/HeaderServlet.java
@@ -13,11 +13,17 @@ public class HeaderServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String h = (String) req.getAttribute("pageHeader");
         resp.getWriter().println("<h1>" + h + "</h1>");
-        resp.getWriter().println("<a href=\"../l1web-app\">index</a>");
-        resp.getWriter().println("<a href=\"/main\">Main</a>");
-        resp.getWriter().println("<a href=\"/catalog\">Catalog</a>");
-        resp.getWriter().println("<a href=\"/product\">Product</a>");
-        resp.getWriter().println("<a href=\"/cart\">Cart</a>");
-        resp.getWriter().println("<a href=\"/order\">Order</a>");
+        resp.getWriter().println("<a href='" + getServletContext().getContextPath() + "/l1web-app'>Index</a>");
+        resp.getWriter().println("<a href='" + getServletContext().getContextPath() + "/main'>Main</a>");
+        resp.getWriter().println("<a href='" + getServletContext().getContextPath() + "/catalog'>Catalog</a>");
+        resp.getWriter().println("<a href='" + getServletContext().getContextPath() + "/product'>Product</a>");
+        resp.getWriter().println("<a href='" + getServletContext().getContextPath() + "/cart'>Cart</a>");
+        resp.getWriter().println("<a href='" + getServletContext().getContextPath() + "/order'>Order</a>");
+//        resp.getWriter().println("<a href=\"../l1web-app\">index</a>");
+//        resp.getWriter().println("<a href=\"/main\">Main</a>");
+//        resp.getWriter().println("<a href=\"/catalog\">Catalog</a>");
+//        resp.getWriter().println("<a href=\"/product\">Product</a>");
+//        resp.getWriter().println("<a href=\"/cart\">Cart</a>");
+//        resp.getWriter().println("<a href=\"/order\">Order</a>");
     }
 }
